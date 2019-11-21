7db765ce9ea9e70bac40f8ae6a2abd62034452a9
KIM HONGIL <clroyan01@gmail.com>
2019-11-21 15:53:10 +0900

    Create oauth2 project

---
+++ src/test/java/boot/security/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,16 @@
+package boot.security.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
